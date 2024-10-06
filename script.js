34ba1ade301511966ac8cd835aee2680bbbdae3d
Jarshan <jarshanjauffar@gmail.com>
2024-10-06 23:07:58 +0530

    display the new added books

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ cancelpopup.addEventListener("click",function(event){
 //select container, add-book, book-title-input, book-author-input, book-description-input
 
 var container = document.querySelector(".container")
-var addbook = document.querySelector(".add-book")
+var addbook = document.getElementById("add-book")
 var booktitleinput = document.getElementById("book-title-input")
 var bookauthorinput = document.getElementById("book-author-input")
 var bookdescriptioninput = document.getElementById("book-description-input")
@@ -31,7 +31,12 @@ addbook.addEventListener("click",function(event){
     event.preventDefault()
     var div=document.createElement("div")
     div.setAttribute("class","book-container")
-    div.innerHTML=`<h1>${booktitleinput.value}</h1>`
+    div.innerHTML=`<h2>${booktitleinput.value}</h2>
+            <h5>${bookauthorinput.value}</h5>
+            <p>${bookdescriptioninput.value}</p>
+            <button>Delete</button>`
     container.append(div)
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
 })
 
